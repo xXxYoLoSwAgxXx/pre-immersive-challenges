4705b356519a005d66ec7f0b87d6268a664408b1
Isaiah Harris <harrisfambam@gmail.com>
2018-06-04 19:20:55 -1000

    complete challenge

--- acceptable-sequence/acceptableSequence.js
+++ acceptable-sequence/acceptableSequence.js
@@ -1,3 +1,21 @@
-module.exports = function acceptableSequence(str) {
-  // write your code here
+module.exports = function acceptableSequence(str){
+  for (let i = 0; i < str.length; i++) {
+    if(str[i].match(/[a-zA-z]/)){
+      if(
+        (str[i+1] !== '+' && str[i-1] !== '+') ||
+        str[i-1] === undefined || 
+        str[i+1] === undefined
+
+
+      ){
+        return false;
+      } else{
+        return true;
+      }
+    }
+  }
 }
+
+
+
+/* function goes through str via for loop then if str[i] which is the letters in str, is surrounded by +, */ 
