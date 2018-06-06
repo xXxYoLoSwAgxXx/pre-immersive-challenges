5b1bb452d47ec009035d7b5fffbdcef150117ed0
Isaiah Harris <harrisfambam@gmail.com>
2018-06-06 00:20:15 -1000

    complete later

--- descending-order/descendingOrder.js
+++ descending-order/descendingOrder.js
@@ -1,3 +1,11 @@
 module.exports = function descendingOrder(number) {
-  // write code in here
+  var str = number.toString();
+  console.log(str);
+  for (let i = 0; i < str.length; i++) {
+    if (typeof number === 'number' === false){
+      console.log('not a number!');
+    } else {
+      console.log(str.split("").sort("").reverse("").join(""));
+  }
 }
+
