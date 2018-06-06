de6ced501923d89345df835f2936da53664da73a
harrisfambam@gmail.com <isaiahharris@Isaiahs-MacBook-Pro.local>
2018-06-05 14:23:57 -1000

    complete cookie challenge

--- cookie-problem/cookieProblem.js
+++ cookie-problem/cookieProblem.js
@@ -1,3 +1,11 @@
 module.exports = function cookieProblem(array) {
-  // write your code in here
+  sortedArray = array.sort();
+  var count = 0;
+  for (let i = 0; i < sortedArray.length; i++) {
+    if (sortedArray[i] < sortedArray[3]){
+      howMuchNeed = sortedArray[3] - sortedArray[i];
+      count += howMuchNeed;
+    }
+  }
+  return count;
 }
