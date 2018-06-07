49b49520ca2d2f40a772e81032de4179df6d4763
Isaiah Harris <harrisfambam@gmail.com>
2018-06-06 22:12:26 -1000

    complete even challenge

--- even-ladder-pattern/evenLadderPattern.js
+++ even-ladder-pattern/evenLadderPattern.js
@@ -1,3 +1,14 @@
 module.exports = function evenLadderPattern(num) {
-  // write your code in here
+  if (num < 1){
+    return "";
+  } else {
+    let arr = [];
+    for (let i = 1; i <= num; i++) {
+      if (i % 2 === 0){
+        var evenStr = i.toString().repeat(i);
+        arr.push(evenStr.toString());
+      } 
+    }
+    return arr.join("\n");
+  }
 }
