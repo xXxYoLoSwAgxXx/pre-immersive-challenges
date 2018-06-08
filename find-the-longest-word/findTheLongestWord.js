848d5e49c3f36dadc35ef0bf29a0ab54f93c0e0e
Isaiah Harris <harrisfambam@gmail.com>
2018-06-08 00:45:07 -1000

    complete longest challenge

--- find-the-longest-word/findTheLongestWord.js
+++ find-the-longest-word/findTheLongestWord.js
@@ -1,3 +1,9 @@
 module.exports = function findTheLongestWord(sen) {
-  // write your code in here
+  let longest = '';
+  for (let i = 0; i < sen.length; i++) {
+    if(sen[i].length > longest.length){
+      longest = sen[i];
+    }
+  }
+  return longest;
 }
