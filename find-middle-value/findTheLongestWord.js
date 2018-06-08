45bf1e3ba0f50b23be3c2c190a7a4b3211da26de
Isaiah Harris <harrisfambam@gmail.com>
2018-06-08 00:44:01 -1000

    delete

--- find-middle-value/findTheLongestWord.js
+++
@@ -1,9 +0,0 @@
-module.exports = function findTheLongestWord(sen) {
-  let longest = '';
-  for (let i = 0; i < sen.length; i++) {
-    if(sen[i].length > longest.length){
-      longest = sen[i];
-    }
-  }
-  return longest;
-}
