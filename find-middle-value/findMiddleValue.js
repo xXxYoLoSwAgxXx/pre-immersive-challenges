221a3665e1590cbb61da014ac7b8980a278e28a3
Isaiah Harris <harrisfambam@gmail.com>
2018-06-08 00:44:32 -1000

    complete middle challenge

--- find-middle-value/findMiddleValue.js
+++ find-middle-value/findMiddleValue.js
@@ -1,3 +1,10 @@
 module.exports = function findMiddleValue(numArray) {
-  // write your code in here
+  let arr = numArray.slice();
+  let sortedArray = numArray.sort(function(a, b){return a-b});
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === sortedArray[1]){
+      return arr.indexOf(arr[i]);
+    }
+    
+  }
 }
