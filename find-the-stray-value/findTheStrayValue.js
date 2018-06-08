e4b45f7f20260b255d6e798796fa43d515b08262
Isaiah Harris <harrisfambam@gmail.com>
2018-06-08 01:21:14 -1000

    complete stray challenge

--- find-the-stray-value/findTheStrayValue.js
+++ find-the-stray-value/findTheStrayValue.js
@@ -1,3 +1,9 @@
 module.exports = function findTheStrayValue(array) {
-  // write your code in here
+let sortedArr = array.sort();
+if (sortedArr[sortedArr.length -1] === sortedArr[sortedArr.length -2] && 
+  sortedArr[0] !== sortedArr[1]){
+    return sortedArr[0];
+}else {
+  return sortedArr[sortedArr.length - 1];
+}
 }
