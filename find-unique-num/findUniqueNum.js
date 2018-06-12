5e52aa5c600d58f21ee2b6cc93fed4f7bff12051
Isaiah Harris <harrisfambam@gmail.com>
2018-06-12 00:44:31 -1000

    complete unique challenge

--- find-unique-num/findUniqueNum.js
+++ find-unique-num/findUniqueNum.js
@@ -1,3 +1,19 @@
 module.exports = function findUniqueNum(strOfNums) {
-  // write your code in here
+var arr = strOfNums.split(" ");
+let even = [];
+let odd = [];
+console.log(arr);
+for (var i = 0; i < arr.length; i++) {
+  if(arr[i] % 2 === 0){
+    even.push(arr[i]);
+  } else {
+    odd.push(arr[i]);
+  }
+
+}
+if (even.length === 1){
+  return arr.indexOf(even[0]);
+} else{
+  return arr.indexOf(odd[0]);
+}
 }
