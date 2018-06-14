9aab61241cb51565d5f556ac29fd34e9524590b5
Isaiah Harris <harrisfambam@gmail.com>
2018-06-13 21:39:19 -1000

    complete insert dashes

--- insert-dashes/insertDashes.js
+++ insert-dashes/insertDashes.js
@@ -1,3 +1,13 @@
 module.exports = function insertDashes(num) {
-  // write your code in here
+  let str = num.toString();
+  let result = [];
+  for (let i = 0; i < str.length; i++) {
+    if(str[i-1] % 2 === 0 && str[i] % 2 === 0){
+      result.push("-", str[i])
+    } else{
+      result.push(str[i]);
+    }
+    
+  }
+  return result.join('');
 }
