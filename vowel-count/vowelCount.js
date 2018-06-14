3e872c4386ea8deadd552dee63e6f0f2535087e8
Isaiah Harris <harrisfambam@gmail.com>
2018-06-14 11:57:07 -1000

    complete vowel count challenge

--- vowel-count/vowelCount.js
+++ vowel-count/vowelCount.js
@@ -1,3 +1,11 @@
 module.exports = function vowelCount(str) {
-  // write your code in here
+  let count = 0;
+  str = str.split('');
+  //console.log(str);
+  for (let i = 0; i < str.length; i++) {
+    if(str[i].match(/[aeiou]/)){
+      count = count + 1;
+    }
+  }
+  return count;
 }
