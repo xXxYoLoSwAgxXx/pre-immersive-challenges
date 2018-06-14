3ff212a1a06c00c6eccfc267ef5caf3c2b695a4b
Isaiah Harris <harrisfambam@gmail.com>
2018-06-14 12:16:43 -1000

    complete swap challenge

--- swap-letter-case/swapLetterCase.js
+++ swap-letter-case/swapLetterCase.js
@@ -1,3 +1,14 @@
 module.exports = function swapLetterCase(str) {
-  // write your code in here
+  str = str.split('');
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toLowerCase()){
+      str[i] = str[i].toUpperCase();
+    } else if (str[i] === str[i].toUpperCase()){
+      str[i] = str[i].toLowerCase();
+    } else {
+      str[i] = str[i]
+    }
+  }
+  console.log(str.join(''));
+  return str.join('');
 }
