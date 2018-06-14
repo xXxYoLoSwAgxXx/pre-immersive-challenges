80a732096a54cb86cd0bca0722a5f37be6173dbb
Isaiah Harris <harrisfambam@gmail.com>
2018-06-14 11:22:10 -1000

    complete reverse challenge

--- reverse-message/reverseMessage.js
+++ reverse-message/reverseMessage.js
@@ -1,3 +1,8 @@
 module.exports = function reverseMessage(str) {
-  // write your code in here
+  str = str.split('').reverse().join('').toLowerCase().split(' ');
+  console.log(str);
+  for (let i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+  }
+  return str.join(' ');
 }
