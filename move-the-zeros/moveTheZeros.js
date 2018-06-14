2db25ec5895fd7764aa8c4025143f42609273b80
Isaiah Harris <harrisfambam@gmail.com>
2018-06-13 21:37:11 -1000

    complete move challenge

--- move-the-zeros/moveTheZeros.js
+++ move-the-zeros/moveTheZeros.js
@@ -1,3 +1,19 @@
-module.exports = function moveTheZeros(arrNum, isRight) {
-  // write your code here
+module.exports = function moveTheZeros (arr, isRight) {
+  if (!!isRight){
+  for(var i = arr.length; i--;) {
+    if(arr[i] === 0) {
+        arr.splice(i, 1);
+        arr.push(0);
+      }
+    }
+  }
+  if(!isRight){
+    for(var k = arr.length -1 ; k>=0;  k--) {
+      if(arr[k] === 0) {
+          arr.splice(k, 1);
+          arr.unshift(0);
+      }
+    }
+  }
+  return arr;
 }
