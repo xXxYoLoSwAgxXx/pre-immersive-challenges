6ad60c3ff9d6c94babbd5350cb3a019cba14798d
Isaiah Harris <harrisfambam@gmail.com>
2018-06-13 21:47:20 -1000

    complete needle challenge

--- needle-in-haystack/needleInHaystack.js
+++ needle-in-haystack/needleInHaystack.js
@@ -1,3 +1,7 @@
 module.exports = function needleInHaystack(haystack) {
-  // write code in here
+  if(haystack.indexOf("needle") > -1){
+    return (haystack.indexOf('needle'));
+  } else {
+    return false;
+  }
 }
